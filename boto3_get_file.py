0cb32a984c69b29018ac9810eb0c21c406fe0ff9
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2025-03-17 11:43:56 -0500

    Update boto3_get_file.py

--- boto3_get_file.py
+++ boto3_get_file.py
@@ -26,7 +26,7 @@ bucket_name = 'g62a5khpnuwmzkoxaziprd4lt4'
 object_key = 'teststse.yaml'
 #host_url = 'https://ociateam.compat.objectstorage.us-ashburn-1.oraclecloud.com'  # Your custom S3 host URL
 hostname = f'https://g62a5khpnuwmzkoxaziprd4lt4.apigateway.us-ashburn-1.oci.customer-oci.com'
-access_key = "34af0a6977a499883e42e81b3e609b61dff93ea3"  # Your AWS Access Key
-secret_key = "cy/o7/K+p79kwcEfSZS+AB+EzDmp+kIUVPonGLF4UH4="  # Your AWS Secret Key
+access_key = "f"  # Your AWS Access Key
+secret_key = "cy/o7/KsfVPonGLF4UH4="  # Your AWS Secret Key
 
 get_s3_file(access_key, secret_key, hostname, bucket_name, object_key, download_path)
